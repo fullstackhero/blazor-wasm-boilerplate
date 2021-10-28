44ca8c109edc6720c9e7f38faebfa4326d7c6e4f
Mukesh Murugan <iammukeshm@gmail.com>
2021-10-29 00:08:20 +0530

    code cleanup

---
+++ src/Client/Theme/CustomColors.cs
@@ -0,0 +1,11 @@
+﻿namespace FSH.BlazorWebAssembly.Client.Theme
+{
+    public static class CustomColors
+    {
+        public static class FSH
+        {
+            public const string Primary = "#3eaf7c";
+            public const string White = "#FFF";
+        }
+    }
+}
---
+++ src/Client/Theme/LightTheme.cs
@@ -0,0 +1,31 @@
+﻿using MudBlazor;
+
+namespace FSH.BlazorWebAssembly.Client.Theme
+{
+    public class LightTheme : MudTheme
+    {
+
+        public LightTheme()
+        {
+            Palette = new Palette()
+            {
+                Primary = CustomColors.FSH.Primary,
+                Secondary = Colors.DeepPurple.Accent2,
+                Background = CustomColors.FSH.White,
+                AppbarBackground = Colors.Blue.Darken1,
+                DrawerBackground = CustomColors.FSH.White,
+                DrawerText = "rgba(0,0,0, 0.7)",
+                Success = "#06d79c"
+            };
+
+            LayoutProperties = new LayoutProperties()
+            {
+                DefaultBorderRadius = "2px",
+            };
+
+            Typography = CustomTypography.FSHTypography;
+            Shadows = new Shadow();
+            ZIndex = new ZIndex();
+        }
+    }
+}
