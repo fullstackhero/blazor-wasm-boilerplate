78708d3e476da072af8a9e7bb87aad10e555f86e
Mukesh Murugan <iammukeshm@gmail.com>
2021-10-28 23:57:26 +0530

    login layout / structure

--- src/Client/Program.cs
+++ src/Client/Program.cs
@@ -1,11 +1,13 @@
 using FSH.BlazorWebAssembly.Client;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MudBlazor.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddMudServices();
 
 await builder.Build().RunAsync();
