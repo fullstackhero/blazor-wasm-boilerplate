c64664c99967e56fcdacb9b740b0ee823c66f923
Mukesh Murugan <iammukeshm@gmail.com>
2022-01-29 20:25:48 +0530

    added comment

--- src/Client.Infrastructure/Common/ApplicationConstants.cs
+++ src/Client.Infrastructure/Common/ApplicationConstants.cs
@@ -17,5 +17,5 @@ public static class ApplicationConstants
     public static readonly string StandardImageFormat = "image/jpeg";
     public static readonly int MaxImageWidth = 1500;
     public static readonly int MaxImageHeight = 1500;
-    public static readonly long MaxAllowedSize = 1000000;
+    public static readonly long MaxAllowedSize = 1000000; // Allows Max File Size of 1 Mb.
 }
