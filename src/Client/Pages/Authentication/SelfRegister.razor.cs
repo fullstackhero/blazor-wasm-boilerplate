6ffb9edccfdad3c265a9de0a0a6d76b75030426a
JB <40014477+274188A@users.noreply.github.com>
2022-02-23 09:49:19 +0800

    user-self register (#134)
    * user-self register
    
    * Bump Mapster from 7.2.0 to 7.3.0 (#135)
    
    Bumps [Mapster](https://github.com/MapsterMapper/Mapster) from 7.2.0 to 7.3.0.
    - [Release notes](https://github.com/MapsterMapper/Mapster/releases)
    - [Commits](https://github.com/MapsterMapper/Mapster/compare/7.2.0...v7.3.0)
    
    ---
    updated-dependencies:
    - dependency-name: Mapster
      dependency-type: direct:production
      update-type: version-update:semver-minor
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    
    * chnages required by change to API (removeal of the self-register API)
    
    * user-self register
    
    * chnages required by change to API (removeal of the self-register API)
    
    * back to using self-register API again after misundertanding on my part
    
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>

---
+++ src/Client/Pages/Authentication/SelfRegister.razor.cs
@@ -0,0 +1,58 @@
+﻿using FSH.BlazorWebAssembly.Client.Components.Common;
+using FSH.BlazorWebAssembly.Client.Infrastructure.ApiClient;
+using FSH.BlazorWebAssembly.Client.Shared;
+using FSH.WebApi.Shared.Multitenancy;
+using Microsoft.AspNetCore.Components;
+using MudBlazor;
+
+namespace FSH.BlazorWebAssembly.Client.Pages.Authentication;
+
+public partial class SelfRegister
+{
+    private readonly CreateUserRequest _createUserRequest = new();
+    private CustomValidation? _customValidation;
+    private bool BusySubmitting { get; set; }
+
+    [Inject]
+    private IUsersClient UsersClient { get; set; } = default!;
+
+    private string Tenant { get; set; } = MultitenancyConstants.Root.Id;
+
+    private bool _passwordVisibility;
+    private InputType _passwordInput = InputType.Password;
+    private string _passwordInputIcon = Icons.Material.Filled.VisibilityOff;
+
+    private async Task SubmitAsync()
+    {
+        BusySubmitting = true;
+
+        string? sucessMessage = await ApiHelper.ExecuteCallGuardedAsync(
+            () => UsersClient.SelfRegisterAsync(Tenant, _createUserRequest),
+            Snackbar,
+            _customValidation);
+
+        if (sucessMessage != null)
+        {
+            Snackbar.Add(sucessMessage, Severity.Info);
+            Navigation.NavigateTo("/login");
+        }
+
+        BusySubmitting = false;
+    }
+
+    private void TogglePasswordVisibility()
+    {
+        if (_passwordVisibility)
+        {
+            _passwordVisibility = false;
+            _passwordInputIcon = Icons.Material.Filled.VisibilityOff;
+            _passwordInput = InputType.Password;
+        }
+        else
+        {
+            _passwordVisibility = true;
+            _passwordInputIcon = Icons.Material.Filled.Visibility;
+            _passwordInput = InputType.Text;
+        }
+    }
+}
