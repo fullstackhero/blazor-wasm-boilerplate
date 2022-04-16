db0db71601e1edeb41c8966d3c38be2198214799
Matthew Corven <matthewcorven@gmail.com>
2022-04-16 17:00:38 -0400

    Powershell scripts should be cross-platform

--- scripts/nswag-regen.ps1
+++ scripts/nswag-regen.ps1
@@ -1,7 +1,9 @@
+# This script is cross-platform, supporting all OSes that PowerShell Core/7 runs on.
+
 $currentDirectory = Get-Location
 $rootDirectory = git rev-parse --show-toplevel
-$hostDirectory = $rootDirectory + '/src/Host'
-$infrastructurePrj = $rootDirectory + '/src/Client.Infrastructure/Client.Infrastructure.csproj'
+$hostDirectory = Join-Path -Path $rootDirectory -ChildPath 'src/Host'
+$infrastructurePrj = Join-Path -Path $rootDirectory -ChildPath 'src/Client.Infrastructure/Client.Infrastructure.csproj'
 
 Write-Host "Make sure you have run the FSH.WebApi project. `n"
 Write-Host "Press any key to continue... `n"
